5a39d0f3504aa55ad1d9ae1da8f8f3facca0bcd0
Huiyeon Kim <hui135@snu.ac.kr>
2025-09-14 12:49:36 +0000

    check

--- pages/2 🙋‍♂️_요청사항_입력.py
+++ pages/2 🙋‍♂️_요청사항_입력.py
@@ -520,7 +520,7 @@ st.html("""
     @media (max-width: 768px) {
         div[data-testid="stHorizontalBlock"] {
             display: grid !important;
-            grid-template-columns: repeat(7, minmax(2000px, 1fr)) !important;
+            grid-template-columns: repeat(7, minmax(200px, 1fr)) !important;
             gap: 0 !important; padding: 0 !important; margin: 0 !important;
             border-top: 1px solid #e0e0e0 !important;
             border-left: 1px solid #e0e0e0 !important;
